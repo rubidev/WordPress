800cdecb6d685f4b543d707a16405b85c9b3d5c6
Lê Văn Toản <levantoan@users.noreply.github.com>
2025-11-09 00:38:58 +0700

    Update row slider js lazyFlickity for row.js

--- Flatsome/row slider js lazyFlickity for row.js
+++ Flatsome/row slider js lazyFlickity for row.js
@@ -1,3 +1,38 @@
+Flatsome.behavior('devvn-row-slider', {
+    attach: function (context) {
+        jQuery('.devvn-row-slider', context).each(function () {
+            var $el = jQuery(this)
+            if ($el.hasClass('devvn-row-slider-js-attached')) return
+            $el.addClass('devvn-row-slider-js-attached')
+            $el.lazyFlickity({
+                "cellAlign": "center",
+                "wrapAround": true,
+                "autoPlay": true,
+                "pauseAutoPlayOnHover": true,
+                "prevNextButtons": false,
+                "percentPosition": true,
+                "imagesLoaded": true,
+                "pageDots": false,
+                "rightToLeft": false,
+                "contain": true
+            })
+            $el.on('flatsome-flickity-ready', function () {
+                var flickity = $el.data('flickity')
+                if (flickity && flickity.player) {
+                    $el.off('mouseenter.flickity mouseleave.flickity')
+                    $el.on('mouseleave', function () {
+                        if (flickity.player && flickity.player.state !== 'playing') {
+                            flickity.player.play()
+                        }
+                    })
+                }
+            })
+        })
+    }
+})
+
+Flatsome.attach('devvn-row-slider');
+
 if($('.devvn_slider_prod').length){
     $('.devvn_slider_prod').each(function(){
         $(this).lazyFlickity({
